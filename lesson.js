ca377d850d29b6c4bedc6ec4104dd51ee3aa6774
yoshiwaki <wakk12107@au.com>
2021-09-30 01:36:33 +0900

    ボタンクリック時の処理と入力した値を繰り返し表示する

--- lesson.js
+++ lesson.js
@@ -2,4 +2,19 @@ const button = document.getElementById('button')
 button.addEventListener('click',function(){
 const fizz = document.getElementById('fizz').value;
 const buzz = document.getElementById('buzz').value;
+
+
+
+// document.getElementById("list").innerHTML = buzz;
+
+function fizzbuzz(){
+  for(fizz; fizz<=100; fizz++){
+      
+      
+document.getElementById("list").innerHTML = fizz;
+  
+  }
+}
+fizzbuzz();
+
 });
