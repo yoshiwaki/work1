b548dfb9fec654f2fe13284512b3e47cec059f2b
yoshiwaki <wakk12107@au.com>
2021-09-29 22:34:27 +0900

    教えていただいたコードの基礎部分

---
+++ lesson.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('button')
+button.addEventListener('click',function(){
+const fizz = document.getElementById('fizz').value;
+const buzz = document.getElementById('buzz').value;
+});
