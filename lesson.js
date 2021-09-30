b01baa101665a172be2cd9433e7e3c3407970b8f
yoshiwaki <wakk12107@au.com>
2021-10-01 01:38:59 +0900

    繰り返しの処理未完成

--- lesson.js
+++ lesson.js
@@ -2,25 +2,29 @@ const button = document.getElementById('button')
 button.addEventListener('click',function(){
 const fizz = document.getElementById('fizz').value;
 const buzz = document.getElementById('buzz').value;
-
+const list = document.getElementById('list');
 
 
 // document.getElementById("list").innerHTML = buzz;
 
 function fizzbuzz(){
   for(i=1; i<=100; i++){
-      if(i%fizz === 0){
+      if(i%fizz == 0){
         // const fizznum = document.getElementById("list") = i+"Fizz";
-        const output = "Fizz"+ i +""
-        document.getElementById("li").innerHTML = output;
+        const newList = document.createElement('li');
+        const output = "Fizz"+ i +"";
+        output.innerHTML;
+        // document.getElementById("li").innerHTML = output;
         
-
-
-
-      }
+        newList.appendChild(output);
+        list.appendChild(newList);
+        }
+        
       
+        
+    
 
-  
+      
   }
 }
 fizzbuzz();
