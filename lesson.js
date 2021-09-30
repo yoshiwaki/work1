ea53b1d52306132c7070686d85a6aa2e6d177848
yoshiwaki <wakk12107@au.com>
2021-09-30 15:49:46 +0900

    for文に変更を加えました

--- lesson.js
+++ lesson.js
@@ -8,10 +8,18 @@ const buzz = document.getElementById('buzz').value;
 // document.getElementById("list").innerHTML = buzz;
 
 function fizzbuzz(){
-  for(fizz; fizz<=100; fizz++){
+  for(i=1; i<=100; i++){
+      if(i%fizz === 0){
+        // const fizznum = document.getElementById("list") = i+"Fizz";
+        const output = "Fizz"+ i +""
+        document.getElementById("li").innerHTML = output;
+        
+
+
+
+      }
       
-      
-document.getElementById("list").innerHTML = fizz;
+
   
   }
 }
