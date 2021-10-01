b160401932f0964bd0cb530ed95e529866202f3f
yoshiwaki <wakk12107@au.com>
2021-10-02 02:49:35 +0900

    条件分岐する以前にiで繰り返された数字の表示ができない

--- lesson.js
+++ lesson.js
@@ -2,23 +2,39 @@ const button = document.getElementById('button')
 button.addEventListener('click',function(){
 const fizz = document.getElementById('fizz').value;
 const buzz = document.getElementById('buzz').value;
-const list = document.getElementById('list');
+// const list = document.getElementById('list');
 
 
 // document.getElementById("list").innerHTML = buzz;
 
 function fizzbuzz(){
-  for(i=1; i<=100; i++){
-      if(i%fizz == 0){
-        // const fizznum = document.getElementById("list") = i+"Fizz";
-        const newList = document.createElement('li');
-        const output = "Fizz"+ i +"";
-        output.innerHTML;
+  for(let i=1; i<=100; i++){
+      if(i%fizz*buzz === 0){
+        
+   
+       const list = document.getElementById('list');
+        // const newList = document.createElement('li');
+        
+        // const output = 
+        // document.innerHTML = output;
+        const list = document.getElementById('list');
+        const lis = document.createElement('li');
+        lis.innerHTML = ("FizzBuzz"+ i +""<br>"");
+        
+       
+
+        // const newList= document.createElement("li");
+        
+        // output.innerHTML;
         // document.getElementById("li").innerHTML = output;
         
-        newList.appendChild(output);
-        list.appendChild(newList);
+        // output.value = "";
+
+
+        // list.appendChild(newList);
+        
         }
+        list.appendChild(lis);
         
       
         
